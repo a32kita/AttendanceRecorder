37e13b56399b88a3caa9af58648eb815d584586c
a32kita <a32kita@yahoo.co.jp>
2018-12-16 12:49:18 +0900

    ModernFrame 定義し直し準備

---
+++ Source/AtRec.Viewer/UI/ModernFrameUI/ModernFrame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtRec.Viewer.UI.ModernFrameUI
+{
+    class ModernFrame
+    {
+    }
+}
